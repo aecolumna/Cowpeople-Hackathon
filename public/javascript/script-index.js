578056d21f772c1034d7fe4a88e8adebbeaeb333
Andres Columna <columnaa@msu.edu>
2019-07-25 15:53:35 -0500

    update

---
+++ public/javascript/script-index.js
@@ -0,0 +1,109 @@
+var my_div = document.getElementById('stressed')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('stressed');
+    my_div.classList.add("rainbow-11");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('empowered')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('empowered');
+    my_div.classList.add("rainbow-11");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+
+
+var my_div = document.getElementById('angry')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('angry');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('joyful')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('joyful');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('indifferent')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('indifferent');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('shameful')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('shameful');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('calm')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('calm');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('sad')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('sad');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('anxious')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('anxious');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('frustrated')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('frustrated');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('bored')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('bored');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
+
+var my_div = document.getElementById('afraid')
+
+var addrainbow = function () {
+    var my_div = document.getElementById('afraid');
+    my_div.classList.add("rainbow-15");
+}
+
+my_div.addEventListener('click', addrainbow)
